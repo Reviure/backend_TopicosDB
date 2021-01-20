365c8428ad4c98ad594c4cd23b57c3c6b92dc663
Jose Francisco Martinez Moreno <josefranciscomartinezmoreno1@gmail.com>
2021-01-20 15:55:37 -0600

    final

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+const {Router} = require('express');
+const router = Router();
+
+
+router.get('/',(req, res)=>{
+    res.json({
+        ok: true,
+        msg: "Recibido"
+    });
+
+});
+
+
+//Crear usuario, login, eliminar //nada se hizo
+
+module.exports = router;
